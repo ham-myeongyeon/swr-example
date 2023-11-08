ca8fc189d858e8d6337009f7cb973ecaee761397
hmy751 <hmy751@gmail.com>
2023-11-09 08:12:56 +0900

    feature: createCacheHelper 메서드 추가

--- internal/utils/createCacheHelper.ts
+++ internal/utils/createCacheHelper.ts
@@ -1,1 +1,25 @@
-export const createCacheHelper = (cache, key) => {};
+import { SWRGlobalState } from "./global-state";
+import type { Cache, State } from "internal/types";
+
+export const createCacheHelper = (
+  cache: Cache,
+  key: string
+): {
+  getCache: () => State;
+  setCache: (value: State) => void;
+} => {
+  const globalState = SWRGlobalState.get(cache);
+  const { setter } = globalState;
+
+  return {
+    getCache: () => cache.get(key) || {},
+    setCache: (value: State) => {
+      if (!key) return;
+
+      const prev = cache.get(key);
+      const merge = { ...prev, ...value };
+
+      setter(key, merge, prev);
+    },
+  };
+};
